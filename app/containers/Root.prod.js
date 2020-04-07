5e722f6413e97cc2bf08b67aa3a687340827c221
Aleksey Kasaev <aleksey.kasaev@gmail.com>
2020-04-07 16:14:10 +0300

    prepare code for production build

--- app/containers/Root.prod.js
+++ app/containers/Root.prod.js
@@ -1,17 +1,47 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 
-import App from '../components/App';
+import { TickerInfo } from '../containers';
+import { Header } from '../components';
+import { PageNotFound } from '../components';
+import { FetchIntervalSelector } from '../containers';
+
+import { tickersList } from '../static/constants';
 
 export default function Root({ store, history }) {
     return (
         <Provider store={store}>
             <div>
                 <ConnectedRouter history={history}>
-                    <Route path="/" component={App} />
+                    <div>
+                        <Header tickers={tickersList} />
+                        <Switch>
+                            <Redirect
+                                exact
+                                from="/"
+                                to={`/${tickersList[0]}`}
+                                key="/"
+                            />
+                            {tickersList.map((ticker) => (
+                                <Route
+                                    exact
+                                    path={`/${ticker}`}
+                                    render={(props) => (
+                                        <TickerInfo
+                                            ticker={ticker}
+                                            {...props}
+                                        />
+                                    )}
+                                    key={ticker}
+                                />
+                            ))}
+                            <Route path="*" key="*" component={PageNotFound} />
+                        </Switch>
+                        <FetchIntervalSelector />
+                    </div>
                 </ConnectedRouter>
             </div>
         </Provider>
